79ea9b2d8b2cee98e1aeb5fe548d4cd5976a75cf
Joanna Jerz <joannajerz1@gmail.com>
2020-04-22 00:01:30 +0200

    feat: basic middleware

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
-import rootReducer from './reducers'
-const store = createStore(rootReducer);
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers';
+
+const logger = store => next => action => {
+  console.group(action.type);
+  console.info('dispatching', action);
+  let result = next(action);
+  console.log("next state", store.getState());
+  console.groupEnd();
+  return result;
+
+}
+
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 ReactDOM.render(
   <React.StrictMode>
